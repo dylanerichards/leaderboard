74f218d57e2ac785d30761fa0bf54e9e21dbdcb8
Dylan Richards <dylanerichards@gmail.com>
2015-05-27 19:19:30 -0400

    Create boilerplate PlayersController

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,17 @@
 class PlayersController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Player.all
+  end
+
+  def create
+    respond_with Player.create(player_params)
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :score)
+  end
 end
