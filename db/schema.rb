b420838abf235a77616eb37d307bc9104684eeae
Dylan Richards <dylanerichards@gmail.com>
2015-05-28 18:49:15 -0400

    Add rank to Player

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527231308) do
+ActiveRecord::Schema.define(version: 20150528224809) do
 
   create_table "players", force: true do |t|
     t.string   "name"
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rank"
   end
 
 end
