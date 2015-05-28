da7cfa1c4cfaf4259d9e0ef8657e053e4e4aef2f
Dylan Richards <dylanerichards@gmail.com>
2015-05-27 21:16:08 -0400

    Remove unnecessary parentheses

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Player do
-  it { should respond_to (:name) }
-  it { should respond_to (:score) }
+  it { should respond_to :name }
+  it { should respond_to :score }
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :score }
