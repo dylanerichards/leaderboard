a74cce180685e6d48045e1aed3465097724acd2f
Dylan Richards <dylanerichards@gmail.com>
2015-05-29 02:53:20 -0400

    Raise Bad Requests when requested size is greater than 100

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,10 +3,12 @@ class PlayersController < ApplicationController
 
   def index
     if params[:offset] && params[:size]
+      raise ActionController::RoutingError.new('Bad Request') if params[:size] > 100
       respond_with Player.all.limit(params[:size].to_i).offset(params[:offset].to_i)
     elsif params[:offset]
       respond_with Player.all.offset(params[:offset].to_i)
     elsif params[:size]
+      raise ActionController::RoutingError.new('Bad Request') if params[:size] > 100
       respond_with Player.all.limit(params[:size].to_i)
     else
       respond_with Player.all.limit(100)
