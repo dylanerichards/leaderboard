22f4a58d0f5498b781ab2a6a768ad925cd195675
Dylan Richards <dylanerichards@gmail.com>
2015-05-29 01:54:56 -0400

    Set and update ranks when a player is added

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,24 @@
 class Player < ActiveRecord::Base
+  after_create :set_rank, :update_ranks
+
   validates_presence_of :name, :score
 
   default_scope { order("score desc") }
 
-  def rank
+  def generate_rank
     scores = Player.all.map(&:score).uniq
-    scores.index(self.score) + 1
+    scores.index(self.score.to_i) + 1
   end
+
+  def set_rank
+    self.rank = generate_rank
+    self.save
+  end
+
+  def update_ranks
+    Player.all.each do |player|
+      player.set_rank
+    end
+  end
+
 end
